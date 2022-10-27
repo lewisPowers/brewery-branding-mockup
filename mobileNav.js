7cbc9d0030eab540fd877a5ccef965f478001cf3
Lewis Powers <lpowers1985@gmail.com>
2022-10-27 13:16:23 -0700

    mobile nav menu

---
+++ mobileNav.js
@@ -0,0 +1,15 @@
+let header = document.getElementsByClassName('header-main')[0];
+const closeBtn = document.getElementsByClassName('close-btn')[0];
+const mobileNavMenu = document.getElementsByClassName('mobile-nav-menu')[0];
+const mobileNav = document.getElementsByClassName('mobile-nav-btn')[0];
+
+
+closeBtn.addEventListener('click', function(e) {
+  mobileNavMenu.style.opacity = '0';
+  mobileNav.style.opacity = '1'
+})
+
+mobileNav.addEventListener('click', function(e) {
+  mobileNavMenu.style.opacity = '1';
+  mobileNav.style.opacity = '0';
+})
--- script.js
+++ script.js
@@ -2,7 +2,6 @@ let dropdownMenu = document.getElementById('nav-menu');
 let gearButton = document.getElementById('menu-example');
 
 gearButton.addEventListener('click', toggleMenu);
-
 function toggleMenu(e) {
   dropdownMenu.classList.toggle('hide');
 }

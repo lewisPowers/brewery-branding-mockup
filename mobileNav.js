07de2335438e4b0822c45585b101d20ebac68085
Lewis Powers <lpowers1985@gmail.com>
2022-10-30 11:47:58 -0700

    mobile menu

--- mobileNav.js
+++ mobileNav.js
@@ -2,7 +2,7 @@ let header = document.getElementsByClassName('header-main')[0];
 const closeBtn = document.getElementsByClassName('close-btn')[0];
 const mobileNavMenu = document.getElementsByClassName('mobile-nav-menu')[0];
 const mobileNav = document.getElementsByClassName('mobile-nav-btn')[0];
-
+const testEl = document.getElementsByClassName('test')[0];
 
 closeBtn.addEventListener('click', function(e) {
   mobileNavMenu.style.opacity = '0';
